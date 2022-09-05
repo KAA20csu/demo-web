5accd3e053b8f2ca2d6fd22c1e99340d1b618ef3
Anny <70793585+anyast103@users.noreply.github.com>
2022-09-05 09:24:38 +0300

    one post demo

--- src/main/java/com/example/demoweb/controller/PostsViewController.java
+++ src/main/java/com/example/demoweb/controller/PostsViewController.java
@@ -13,5 +13,9 @@ public class PostsViewController {
     public String list() {
         return "Здесь будет главная страница";
     }
-
+    @ResponseBody
+    @RequestMapping(path = "/post/{id}", method = RequestMethod.GET)
+    public String single(@PathVariable("id") Long id) {
+        return "Здесь будет страница поста №" + id;
+    }
 }
